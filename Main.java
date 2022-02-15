2fc6942b7330bb954377919758bc4bf5da4b7b09
Jeewantha Udeshika Ariyawansha <e18028@eng.pdn.ac.lk>
2022-02-15 11:39:04 +0530

    Autor name is changed

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+CO225 - Software Construction
+Lab06 - Event Handling and MVC Approch
+
+This is Main class of Tic Tac Toe game
+
+Author : Ariyawansha P.H.J.U. (E/18/028)
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Invoke Gameview to start the game
+        new GameView();
+    }
+}
